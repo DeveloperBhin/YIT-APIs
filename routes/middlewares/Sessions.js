739b927545642d19b453fadcfbc6adc17c9c55e2
DeveloperBhin <norbeth559@gmail.com>
2025-12-23 11:07:23 +0300

    together we can

--- routes/middlewares/Sessions.js
+++ routes/middlewares/Sessions.js
@@ -1,4 +1,4 @@
-const  redis = require('../../redis');
+// const  redis = require('../../redis');
 const  { v4 : uuidv4 } = require( 'uuid');
 const db  = require  ('../../database');
 const crypto  = require ('crypto');
