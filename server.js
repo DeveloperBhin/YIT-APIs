b23021cfd5d2f9a3f47b9cb844c300381024d823
DeveloperBhin <norbeth559@gmail.com>
2025-12-23 10:25:58 +0300

    together we can

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const {
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || 'https://yit-web.deploy.tz',
+    origin: process.env.FRONTEND_URL || 'https://yit-web.deploy.tz/',
     methods: ['GET', 'POST'],
     credentials: true,
   },
